1766f9ffa588ea3af44d3ee1ddd2cf14ffd1ae0b
Raja Puranam <raja.puranam@appsassoc.com>
2021-09-17 20:31:23 +0530

    Encrypt-Decrypt functions added

--- hash_password.py
+++ hash_password.py
@@ -21,20 +21,20 @@ def encrypt(plain_text):
 	cipher_text = ''.join(padded_2)
 	return cipher_text
 
-# def decrypt(cipher_text):
-# 	d1, d2 = '', []
-# 	for i in range(0, len(cipher_text), 8):
-# 		d1 += (cipher_text[i:i+6])
-# 	for i in range(0, len(d1), 6):
-# 		d2.append(d1[i:i+3])
+def decrypt(cipher_text):
+	d1, d2 = '', []
+	for i in range(0, len(cipher_text), 8):
+		d1 += (cipher_text[i:i+6])
+	for i in range(0, len(d1), 6):
+		d2.append(d1[i:i+3])
 
-# 	if len(d2) % 2 == 0:
-# 		ind = len(d2) // 2
-# 	else:
-# 		ind = (len(d2) // 2)+1
+	if len(d2) % 2 == 0:
+		ind = len(d2) // 2
+	else:
+		ind = (len(d2) // 2)+1
 
-# 	ascii_values = d2[ind:] + d2[:ind]
-# 	plain_text = ''.join([chr(int(v[::-1])) for v in ascii_values])
+	ascii_values = d2[ind:] + d2[:ind]
+	plain_text = ''.join([chr(int(v[::-1])) for v in ascii_values])
 
-# 	return plain_text
+	return plain_text
 
